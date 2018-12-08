324dd46a3f7fadfe6e1e3ff86daebe470c40a609
H4wki <realisvenator@gmail.com>
2018-12-08 17:02:32 -0600

    Still broken

--- b+tree/bplustree.h
+++ b+tree/bplustree.h
@@ -1,35 +1,30 @@
-#ifndef BPLUSHEADER
-#define BPLUSHEADER
-
 #pragma once
 #include <string>
-
 class bplustree
 {
-public:
-	bplustree();
-	bool insert();
-	bool remove();
-	bool checkEmpty();
-	bool isPresent();
-	int size();
-private:
-	//Header Record Variables
-	int headerSize;
-	int blockSize;
-	int totalBlocks;
-	int numAvalBlocks;//Number of open blocks
-	int numOfLevels;
-	int rbnRoot; //Relative offest to root node
-	int rbnLevels;//Relative offset to levels
-	int rbnAvailList;//Relative offset to the avial list
-	int totalNumRecords;//total number of records
-	int sizeofRecords;
-	int numFieldsPerRecord;
-	// String variable
-	std::string description;
-	std::string time;
-	std::string lastMod;
+	private:
+		//Header Record Variables
+		int headerSize;
+		int blockSize;
+		int totalBlocks;
+		int numAvalBlocks;//Number of open blocks
+		int numOfLevels;
+		int rbnRoot; //Relative offest to root node
+		int rbnLevels;//Relative offset to levels
+		int rbnAvailList;//Relative offset to the avial list
+		int totalNumRecords;//total number of records
+		int sizeofRecords;
+		int numFieldsPerRecord;
+		// String variable
+		std::string description;
+		std::string time;
+		std::string lastMod;
+	public:
+		bplustree();
+		bool insert();
+		bool remove();
+		bool checkEmpty();
+		bool isPresent();
+		int size();
 };
 
-#endif
--- b+tree/main.cpp
+++ b+tree/main.cpp
@@ -1,6 +1,5 @@
 #include "bplustree.cpp"
 #include <iostream>
-
 int main() {
 	bplustree test1;
 	std::cout << test1.size();
